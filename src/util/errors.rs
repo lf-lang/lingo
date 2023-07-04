e1921b05042874bcefa79f2364e845c6e15b0af9
Clément Fournier <clement.fournier@tu-dresden.de>
2023-07-04 12:51:06 +0200

    Remove composite error

--- src/util/errors.rs
+++ src/util/errors.rs
@@ -10,7 +10,6 @@ pub type BuildResult = Result<(), Box<AnyError>>;
 
 #[derive(Debug)]
 pub enum LingoError {
-    Composite(Vec<Box<AnyError>>),
     Shared(Arc<AnyError>),
     CommandFailed(Command, ExitStatus),
     UnknownAppNames(Vec<String>),
@@ -18,43 +17,9 @@ pub enum LingoError {
     UseWestBuildToBuildApp,
 }
 
-/// Merge two build results into one, collecting errors.
-pub fn merge(b1: BuildResult, b2: BuildResult) -> BuildResult {
-    match (b1, b2) {
-        (Ok(()), Ok(())) => Ok(()),
-        (Ok(()), e) | (e, Ok(())) => e,
-        (Err(mut e1), Err(mut e2)) => {
-            match (
-                e1.downcast_mut::<LingoError>(),
-                e2.downcast_mut::<LingoError>(),
-            ) {
-                (Some(LingoError::Composite(vec)), Some(LingoError::Composite(vec2))) => {
-                    vec.append(vec2);
-                    Err(e1)
-                }
-                (Some(LingoError::Composite(vec)), _) => {
-                    vec.push(e2);
-                    Err(e1)
-                }
-                (_, Some(LingoError::Composite(vec))) => {
-                    vec.push(e1);
-                    Err(e2)
-                }
-                (_, _) => Err(Box::new(LingoError::Composite(vec![e1, e2]))),
-            }
-        }
-    }
-}
-
 impl Display for LingoError {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         match self {
-            LingoError::Composite(errors) => {
-                for error in errors {
-                    writeln!(f, "{}", error)?
-                }
-                Ok(())
-            }
             LingoError::Shared(err) => {
                 write!(f, "{}", err)
             }
